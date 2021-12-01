434f8736d836bb43be45bb7760a49d7953eea8cb
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2021-12-01 23:43:18 +0900

    dockerで開発環境を立ち上げ

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+# rm -f /coffee_passport/pids/server.pid  mysql8.0の設定
+rm -f /ideaFrameworks/tmp/pids/server.pid
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
