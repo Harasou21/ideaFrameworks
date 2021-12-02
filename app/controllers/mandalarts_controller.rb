cce85be54bd46fb21d5a3124aa7762e3317b32b0
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2021-12-02 18:07:15 +0900

    mandalarts#new,#createのVCを作成

--- app/controllers/mandalarts_controller.rb
+++ app/controllers/mandalarts_controller.rb
@@ -3,5 +3,22 @@ class MandalartsController < ApplicationController
     @mandalarts = Mandalart.all
   end
 
-  
+  def new
+    @mandalart = Mandalart.new
+  end
+
+  def create
+    @mandalart = Mandalart.new(mandalart_params)
+    if @mandalart.save
+      redirect_to mandalarts_path
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+    def mandalart_params
+      params.require(:mandalart).permit(:text)
+    end
 end
