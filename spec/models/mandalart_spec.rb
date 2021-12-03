c890fdd50cae2a25a34dad542ec6c3201ddecacd
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2021-12-03 22:25:46 +0900

    mandalart.rbの単体テストを記述

--- spec/models/mandalart_spec.rb
+++ spec/models/mandalart_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Mandalart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @mandalart = FactoryBot.build(:mandalart)
+  end
+
+  describe "投稿" do
+    context "投稿できる場合" do
+      it "テキストがあれば投稿できる" do
+        expect(@mandalart).to be_valid
+      end
+    end
+
+    context "投稿できない場合" do
+      it "テキストの文字数が300文字以上" do
+        @mandalart.text = "a"* 301
+        @mandalart.valid?
+        expect(@mandalart.errors.full_messages).to include("Text is too long (maximum is 300 characters)")
+      end
+    end
+
+  end
 end
