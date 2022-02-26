1f519c4652f7abc093117beb0c03d219872fdfd3
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-02-26 21:05:20 +0900

    Userモデルのdbを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_133426) do
+ActiveRecord::Schema.define(version: 2021_12_24_143407) do
 
   create_table "mandalarts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "text"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2021_12_13_133426) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "x"
     t.integer "y"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
