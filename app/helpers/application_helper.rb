6c0ea2842b598f4fcdd2073755c4efc5198aa6e7
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-03-21 17:01:25 +0900

    バリデーションエラーメッセージを表示するメソッドを定義 #5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def display_error_messages(model,attribute)
+    model.errors.full_messages_for(attribute).each do |error|
+      @error = error
+    end
+    @error
+  end
 end
