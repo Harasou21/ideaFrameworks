bfb64d8acbcc62aeeab5f10650035d83c7cc788a
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-03-21 17:28:44 +0900

    自作ヘルパーメソッドの不具合を修正 #5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def display_error_messages(model,attribute)
+    @error = nil
+    # インスタンスの中身を一旦削除
+    # 削除しないと2回目呼ばれたときに、インスタンスの中身を
+    # そのまま使用してしまい、違うフォームで別のエラーメッセージが
+    # 出力されてしまう。
     model.errors.full_messages_for(attribute).each do |error|
       @error = error
     end
