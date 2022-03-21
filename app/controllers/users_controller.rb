0cb5b452407c2c6f0efde0903365dd9f2eaaae24
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-03-21 17:00:40 +0900

    ユーザー登録のコントローラーを記述 #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  before_action :find_user, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if  @user.save
+      
+    else
+      render  'new'
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name,:email,:password,
+                                    :password_confirmation)
+    end
+
 end
