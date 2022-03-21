362119e75d1658567e1c47cba03d3fd14f543ca3
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-03-21 17:32:39 +0900

    登録後にusers/show.html.erbに遷移 #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if  @user.save
-      
+      redirect_to @user
     else
       render  'new'
     end
