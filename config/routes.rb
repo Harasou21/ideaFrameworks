e6462852a3a720b8afe2e7409f45d7a40afefe97
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-04-02 16:03:42 +0900

    sessionsのルーティングを作成 #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "mandalarts#index"
   resources :mandalarts
@@ -10,4 +9,8 @@ Rails.application.routes.draw do
     resources :mindmaps
   end
   resources :users
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete 'logout',  to: 'sessions#destroy'
 end
