587b2940022d4d7d30bfbd2d9b7f0c3e6875eb68
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-04-24 13:48:39 +0900

    ログイン機構を作成 #9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    password = params[:session][:password]
+    if user && user.authenticate(password)
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'パスワードとemailの組み合わせが正しくありません'
+      render 'new'
+    end
   end
 end
