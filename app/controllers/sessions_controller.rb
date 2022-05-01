f0b308317a904d62d5f697300db41f86347f6427
harasou41soccer@gmail.com <harasou41soccer@gmail.com>
2022-05-01 12:13:10 +0900

    ログイン、ログアウト機能作成 #9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,21 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
-  
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     password = params[:session][:password]
-    if user && user.authenticate(password)
+    if user&.authenticate(password)
       log_in user
       redirect_to user
     else
       flash[:danger] = 'パスワードとemailの組み合わせが正しくありません'
       render 'new'
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
